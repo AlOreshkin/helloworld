eff9f4295f4f20e3d06c2894759eb4a0ee7768cf
Александр Орешкин <aoresh@Mac-mini-Aleksandr.local>
2025-04-29 22:39:03 +0300

    blank project created

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Александр Орешкин on 26.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
